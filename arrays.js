41e6047b8708464e92185ead855bd79fd18076dc
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-09 22:49:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray() {
+  var cities = ["New York", "Chicago", "Detriot"]
+  cities = ["Dublin" ...cities]
+}
