71caa3b7690c81cdf1961dbfe6501807c1e2bb7b
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-09 23:14:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-var candyString =`foo`
+var candyString =`1foo`
 
 function addElementToBeginningOfArray(chocolateBars, candyString){
 return [candyString, ...chocolateBars]
