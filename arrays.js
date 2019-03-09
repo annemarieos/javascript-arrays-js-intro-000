eb7733c6029cce06304cbaf32c73898cb96780e9
ANNEMARIE OSULLIVAN <annemarieos.irl@gmail.com>
2019-03-09 23:43:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,3 +32,8 @@ function removeElementFromBeginningOfArray(chocolateBars) {
   chocolateBars.slice(1)
   chocolateBars
 }
+
+function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
+  chocolateBars = chocolateBars.slice(-1)
+  chocolateBars
+}
